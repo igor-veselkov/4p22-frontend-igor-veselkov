3d7dbf7cedd2e55092e58ed339ad1c1e7e3886ed
igor-veselkov <veselkov_igor@bk.ru>
2022-11-01 19:46:00 +0500

    changing 'a' var

--- homework_9/script.js
+++ homework_9/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function makeFibonacciFunction() {
-    let a = 1;
+    let a = 0;
     let b = 1;
 
     function result() {
@@ -16,8 +16,10 @@ function makeFibonacciFunction() {
 
 const fibonacci = makeFibonacciFunction();
 
+console.log(fibonacci()); // Вывод в консоль: 0
 console.log(fibonacci()); // Вывод в консоль: 1
 console.log(fibonacci()); // Вывод в консоль: 1
 console.log(fibonacci()); // Вывод в консоль: 2
 console.log(fibonacci()); // Вывод в консоль: 3
 console.log(fibonacci()); // Вывод в консоль: 5
+console.log(fibonacci()); // Вывод в консоль: 8
